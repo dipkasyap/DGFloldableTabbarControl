66f1e2c75d1dd421f2f9ee6a69e9186c13cb4047
dpd Ghimire (dip kasyap) <dpd.ghimire@gmail.com>
2017-09-05 17:36:56 +0545

    Initial Commit

---
+++ DGFloldableTabbarControl/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DGFloldableTabbarControl
+//
+//  Created by Apple on 9/5/17.
+//  Copyright © 2017 dip. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
